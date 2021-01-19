adee8ba6c9b13a230bf3f5f537b4e4f445ce6909
chrisocodes <christopherocodes@gmail.com>
2021-01-18 23:15:47 -0500

    update file to include sequence text

--- src/observatoryData.js
+++ src/observatoryData.js
@@ -5,12 +5,36 @@ const observatoryData = {
     thumb_filename: "thumb.jpeg",
     video_url_id: "288344114",
     sequence: {
-      a: "a.jpg",
-      b: "b.jpg",
-      c: "c.jpg",
-      d: "d.jpg",
-      e: "e.jpg",
-      f: "f.jpg",
+      slide_1: {
+        slide_image: "a.jpg",
+        text:
+          "in observatory i sunlight is radiating on an empty tunnel projecting a ring of light",
+      },
+      slide_2: {
+        slide_image: "b.jpg",
+        text:
+          "this luminous ring ocilates between my face and the face on the other end of the tunnel",
+      },
+      slide_3: {
+        slide_image: "c.jpg",
+        text:
+          "within this radiant ring i see the emptiness of space and the face on the other end of the tunnel",
+      },
+      slide_4: {
+        slide_image: "d.jpg",
+        text:
+          "within this radiant ring i see the emptiness of space flowing through my face",
+      },
+      slide_5: {
+        slide_image: "e.jpg",
+        text:
+          "suddenly i see that my end of the tunnel is utterly empty of my face",
+      },
+      slide_6: {
+        slide_image: "f.jpg",
+        text:
+          "here at zero distance i am seeing the empty space i am looking out from",
+      },
     },
   },
   ii: {
@@ -19,12 +43,36 @@ const observatoryData = {
     thumb_filename: "thumb.jpeg",
     video_url_id: "288344114",
     sequence: {
-      a: "a.jpg",
-      b: "b.jpg",
-      c: "c.jpg",
-      d: "d.jpg",
-      e: "e.jpg",
-      f: "f.jpg",
+      slide_1: {
+        slide_image: "a.jpg",
+        text:
+          "in observatory i sunlight is radiating on an empty tunnel projecting a ring of light",
+      },
+      slide_2: {
+        slide_image: "b.jpg",
+        text:
+          "this luminous ring ocilates between my face and the face on the other end of the tunnel",
+      },
+      slide_3: {
+        slide_image: "c.jpg",
+        text:
+          "within this radiant ring i see the emptiness of space and the face on the other end of the tunnel",
+      },
+      slide_4: {
+        slide_image: "d.jpg",
+        text:
+          "within this radiant ring i see the emptiness of space flowing through my face",
+      },
+      slide_5: {
+        slide_image: "e.jpg",
+        text:
+          "suddenly i see that my end of the tunnel is utterly empty of my face",
+      },
+      slide_6: {
+        slide_image: "f.jpg",
+        text:
+          "here at zero distance i am seeing the empty space i am looking out from",
+      },
     },
   },
   iii: {
@@ -33,27 +81,79 @@ const observatoryData = {
     thumb_filename: "thumb.jpeg",
     video_url_id: "288344114",
     sequence: {
-      a: "a.jpg",
-      b: "b.jpg",
-      c: "c.jpg",
-      d: "d.jpg",
-      e: "e.jpg",
-      f: "f.jpg",
+      slide_1: {
+        slide_image: "a.jpg",
+        text:
+          "in observatory i sunlight is radiating on an empty tunnel projecting a ring of light",
+      },
+      slide_2: {
+        slide_image: "b.jpg",
+        text:
+          "this luminous ring ocilates between my face and the face on the other end of the tunnel",
+      },
+      slide_3: {
+        slide_image: "c.jpg",
+        text:
+          "within this radiant ring i see the emptiness of space and the face on the other end of the tunnel",
+      },
+      slide_4: {
+        slide_image: "d.jpg",
+        text:
+          "within this radiant ring i see the emptiness of space flowing through my face",
+      },
+      slide_5: {
+        slide_image: "e.jpg",
+        text:
+          "suddenly i see that my end of the tunnel is utterly empty of my face",
+      },
+      slide_6: {
+        slide_image: "f.jpg",
+        text:
+          "here at zero distance i am seeing the empty space i am looking out from",
+      },
     },
   },
   iv: {
     id: "iv",
-    logo_file: "logo.svg",
+    logo_filename: "logo.svg",
     thumb_filename: "thumb.jpeg",
     video_url_id: "288344114",
     sequence: {
-      a: "a.jpg",
-      b: "b.jpg",
-      c: "c.jpg",
-      d: "d.jpg",
-      e: "e.jpg",
-      f: "f.jpg",
-      g: "g.jpg",
+      slide_1: {
+        slide_image: "a.jpg",
+        text:
+          "in observatory i sunlight is radiating on an empty tunnel projecting a ring of light",
+      },
+      slide_2: {
+        slide_image: "b.jpg",
+        text:
+          "this luminous ring ocilates between my face and the face on the other end of the tunnel",
+      },
+      slide_3: {
+        slide_image: "c.jpg",
+        text:
+          "within this radiant ring i see the emptiness of space and the face on the other end of the tunnel",
+      },
+      slide_4: {
+        slide_image: "d.jpg",
+        text:
+          "within this radiant ring i see the emptiness of space flowing through my face",
+      },
+      slide_5: {
+        slide_image: "e.jpg",
+        text:
+          "suddenly i see that my end of the tunnel is utterly empty of my face",
+      },
+      slide_6: {
+        slide_image: "f.jpg",
+        text:
+          "here at zero distance i am seeing the empty space i am looking out from",
+      },
+      slide_7: {
+        slide_image: "g.jpg",
+        text:
+          "here at zero distance i am seeing the empty space i am looking out from",
+      },
     },
   },
   v: {
@@ -62,13 +162,41 @@ const observatoryData = {
     thumb_filename: "thumb.jpeg",
     video_url_id: "288344114",
     sequence: {
-      a: "a.jpg",
-      b: "b.jpg",
-      c: "c.jpg",
-      d: "d.jpg",
-      e: "e.jpg",
-      f: "f.jpg",
-      g: "g.jpg",
+      slide_1: {
+        slide_image: "a.jpg",
+        text:
+          "in observatory i sunlight is radiating on an empty tunnel projecting a ring of light",
+      },
+      slide_2: {
+        slide_image: "b.jpg",
+        text:
+          "this luminous ring ocilates between my face and the face on the other end of the tunnel",
+      },
+      slide_3: {
+        slide_image: "c.jpg",
+        text:
+          "within this radiant ring i see the emptiness of space and the face on the other end of the tunnel",
+      },
+      slide_4: {
+        slide_image: "d.jpg",
+        text:
+          "within this radiant ring i see the emptiness of space flowing through my face",
+      },
+      slide_5: {
+        slide_image: "e.jpg",
+        text:
+          "suddenly i see that my end of the tunnel is utterly empty of my face",
+      },
+      slide_6: {
+        slide_image: "f.jpg",
+        text:
+          "here at zero distance i am seeing the empty space i am looking out from",
+      },
+      slide_7: {
+        slide_image: "g.jpg",
+        text:
+          "here at zero distance i am seeing the empty space i am looking out from",
+      },
     },
   },
 };
