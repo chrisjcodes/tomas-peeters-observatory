dd4275b2449c4654341dea19c365ae4b25b72af0
Chris Johnson <chrisjohnson.dev.la@gmail.com>
2021-04-19 22:09:34 -0700

    Implement triptychs slider

--- src/views/Triptychs.vue
+++ src/views/Triptychs.vue
@@ -1,30 +1,107 @@
 <template>
   <div>
-    <section>
-      <header>
-        <img src="@/assets/triptychs/i/logo.svg" />
-      </header>
-      <div class="panel-container">
-        <div class="panel-item">
-          <img src="@/assets/triptychs/i/a.jpg" />
+    <VueSlickCarousel v-bind="slickSettings">
+      <section v-for="triptych in triptychs" :key="triptych.name">
+        <header>
+          <img
+            draggable="false"
+            :src="require(`@/assets/triptychs/${triptych.name}/logo.svg`)"
+          />
+        </header>
+        <div class="panel-container">
+          <div class="panel-item">
+            <img
+              draggable="false"
+              :src="require(`@/assets/triptychs/${triptych.name}/a.jpg`)"
+            />
+          </div>
+          <div class="panel-item">
+            <img
+              draggable="false"
+              :src="require(`@/assets/triptychs/${triptych.name}/b.jpg`)"
+            />
+          </div>
+          <div class="panel-item">
+            <img
+              draggable="false"
+              :src="require(`@/assets/triptychs/${triptych.name}/c.jpg`)"
+            />
+          </div>
         </div>
-        <div class="panel-item">
-          <img src="@/assets/triptychs/i/b.jpg" />
+        <div class="copy-container">
+          <p>What you are looking for is your single eye</p>
         </div>
-        <div class="panel-item">
-          <img src="@/assets/triptychs/i/c.jpg" />
-        </div>
-      </div>
-      <div class="copy-container">
-        <p>What you are looking for is your single eye</p>
-      </div>
-    </section>
+      </section>
+    </VueSlickCarousel>
   </div>
 </template>
 
 <script>
-export default {};
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+
+export default {
+  components: {
+    VueSlickCarousel,
+  },
+  data() {
+    return {
+      slickSettings: {
+        arrows: false,
+        dots: true,
+        dotsClass: "slick-dots triptych-dots",
+        infinite: true,
+        fade: true,
+        speed: 3500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+      triptychs: [
+        {
+          name: "i",
+          copy: "WHAT YOU ARE LOOKING FOR IS YOUR SINGLE EYE",
+        },
+        {
+          name: "ii",
+          copy: "WHAT YOU ARE LOOKING FOR IS YOUR SINGLE EYE",
+        },
+        {
+          name: "iii",
+          copy: "WHAT YOU ARE LOOKING FOR IS YOUR SINGLE EYE",
+        },
+        {
+          name: "iv",
+          copy: "WHAT YOU ARE LOOKING FOR IS YOUR SINGLE EYE",
+        },
+      ],
+    };
+  },
+};
 </script>
+
+<style lang="scss">
+.slick-slide * {
+  outline: none;
+}
+
+.triptych-dots {
+  display: flex;
+  position: absolute !important;
+  bottom: -30px;
+
+  & li.slick-active button:before {
+    opacity: 0.55;
+    color: white;
+  }
+
+  & li button:before {
+    opacity: 0.15;
+    color: white;
+    font-size: 8px;
+  }
+}
+</style>
 
 <style lang="scss" scoped>
 @import "@/theme/colors.scss";
@@ -33,7 +110,7 @@ export default {};
 @import "@/theme/typography.scss";
 
 section {
-  padding-top: rem(60px);
+  padding-top: rem(50px);
 }
 
 header {
@@ -53,7 +130,7 @@ header {
   grid-gap: 5px;
   margin-left: 10vw;
   margin-right: 10vw;
-  margin-bottom: rem(50px);
+  margin-bottom: rem(40px);
 }
 
 .panel-item {
